e39340149c607e1cdd509d8492f5d8c82c4e682c
AndreaCremonesi4 <114904033+AndreaCremonesi4@users.noreply.github.com>
2023-06-15 21:45:46 +0200

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const quote = require('./models/quote');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const quotes = [];
+var quotes = [];
 
 const connectDB = async() => {
     try{
@@ -32,7 +32,7 @@ app.get('/api', (req, res) => {
 
 connectDB().then(() => {
     quotes = quote.find();
-    
+
     app.listen(port, () => {
         console.log(`Listening on port ${port}`)
     })
