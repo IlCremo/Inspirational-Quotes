0964de9e16b219ec9ebb3927bac155e90b59fbce
AndreaCremonesi4 <114904033+AndreaCremonesi4@users.noreply.github.com>
2023-06-15 19:37:32 +0200

    added API

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+const rawdata = fs.readFileSync('../data/data.json');
+const quotes = JSON.parse(rawdata);
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.get('/api', (req, res) => {
+    let r = Math.floor(Math.random() * quotes.length);
+    let quote = quotes[r];
+    res.json(quote);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
